59ae3e30af44fe3682ffb45098ab4e5dd8b6388b
Alexandre POIRRIER <alex.poirrier@gmail.com>
2020-10-03 12:10:11 +0200

    try to solved the navbar problem, work in progress

--- script.js
+++ script.js
@@ -2,15 +2,30 @@ const down = document.getElementById('burger_btn_down');
 const up = document.getElementById('burger_btn_up');
 const responsiveMenu = document.getElementById('responsive_menu');
 
+// down.addEventListener('click', function(){
+//     responsiveMenu.style.display = "flex";
+//     down.style.zIndex = 1;
+//     up.style.zIndex = 2;
+// })
+
+// up.addEventListener('click', function(){
+//     responsiveMenu.style.display = "none";
+//     down.style.zIndex = 2;
+//     up.style.zIndex = 1;
+    
+// })
+
+
+
+
 down.addEventListener('click', function(){
-    responsiveMenu.style.display = "flex";
+    responsiveMenu.style.marginTop = "300px";
     down.style.zIndex = 1;
     up.style.zIndex = 2;
 })
 
 up.addEventListener('click', function(){
-    responsiveMenu.style.display = "none";
+    responsiveMenu.style.height = 0;
     down.style.zIndex = 2;
-    up.style.zIndex = 1;
-    
+    up.style.zIndex = 1;    
 })
