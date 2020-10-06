cb83cf71b6ffe3284bc6f311321277abd00438bd
Alexandre POIRRIER <alex.poirrier@gmail.com>
2020-10-06 14:36:54 +0200

    ADD message on submit

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const dateTime = document.getElementById('date_time_box');
 const nbPers = document.getElementById('nb_personnes');
 const other = document.getElementById('other');
 const chooseMotif = document.getElementById('choose_motif');
+const date = document.getElementById('date').value;
+const time = document.getElementById('time').value;
 
 select.addEventListener('change', function(){
     if (select.value =="reservation"){
@@ -22,5 +24,11 @@ const submit = document.getElementById('submit_btn');
 const form = document.getElementById('form');
 
 form.onsubmit = function (){
-    alert('ok');
+    if (select.value === "reservation"){
+        alert (`Votre ${select.value} du ${date} à ${time} pour ${nbPers.value} personnes a bien été reçu.`)
+    }else if (select.value === "other"){
+        alert (`Votre ${chooseMotif.value} à bien été reçu.`)
+    }else{
+        alert(`Votre ${select.value} a bien été reçu.`);
+    }    
 }
